4b6ef7d553b22e2de882c194c6c57121dd07bac2
Forrest Eastland <feastland4@gmail.com>
2020-02-11 03:15:52 -0800

    Updates

---
+++ precache-manifest.583cc3ef8bb61690d0853d5acdfcebca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f4e3599451b7d39cb423e0b278da82",
+    "url": "/hooks-app/index.html"
+  },
+  {
+    "revision": "f048c238023e5ee313f5",
+    "url": "/hooks-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "da364f7ee486b42d6a3f",
+    "url": "/hooks-app/static/css/main.9be421e6.chunk.css"
+  },
+  {
+    "revision": "f048c238023e5ee313f5",
+    "url": "/hooks-app/static/js/2.b891de37.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/hooks-app/static/js/2.b891de37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da364f7ee486b42d6a3f",
+    "url": "/hooks-app/static/js/main.6adf5f8b.chunk.js"
+  },
+  {
+    "revision": "61053ff9d30f21db9c3a",
+    "url": "/hooks-app/static/js/runtime-main.de208bba.js"
+  }
+]);
